71e403254781330f33e0868899fa474edafdfd03
Tanner Heffner & Connor Abdelnoor & Tom Mertz <student@epicodus.com>
2015-04-29 11:14:01 -0700

    Add gameOver condition

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 var square;
 var score = 0;
-var timer = 30;
+var timer = 3;
+var gameOver = false;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
@@ -11,21 +12,29 @@ function setup() {
 }
 
 function myTimer() {
-    timer -= 1;
+    if( timer > 0) {
+        timer -= 1;
+    } else {
+        gameOver = true;
+    }
 }
 
 function draw() {
-    background(0);
-    square.move();
-    square.display();
-    text("score: " + score, 50, windowHeight - 50);
-    text("Timer: " + timer, windowWidth - 200, windowHeight - 50)
+
+    if(!gameOver) {
+        background(0);
+        square.move();
+        square.display();
+        text("score: " + score, 50, windowHeight - 50);
+        text("Timer: " + timer, windowWidth - 200, windowHeight - 50)
+    }
 }
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
     background(0);
     textSize(32);
+    square.display();
     text("score: " + score, 50, windowHeight - 50);
     text("Timer: " + timer, windowWidth - 150, windowHeight - 50)
 }
@@ -41,6 +50,7 @@ function Target(){
 
     this.move = function() {
 
+        //prevents the box from going off the screen
         if (this.x > windowWidth - this.diameter) {
             //add slight variation
             this.Xspeed *= -1;
@@ -59,15 +69,15 @@ function Target(){
             this.y = 1;
         }
 
+        //checks if the mouse is inside the box
+        //gives points if it is
         if (mouseX >= this.x && mouseX <= this.x + this.diameter && mouseY >= this.y && mouseY <= this.y + this.diameter ) {
             this.Xspeed *= (1 + random(0, this.variation));
             this.Yspeed *= (1 + random(0, this.variation));
             score++;
         }
 
-
-
-
+        //caps the speed at maxSpeed
         if(this.Xspeed > this.maxSpeed) {
             this.Xspeed = this.maxSpeed;
         }
