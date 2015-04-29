ef131dc1b221c0603330d941dbc8ff70efbb7604
Tanner Heffner & Connor Abdelnoor & Tom Mertz <student@epicodus.com>
2015-04-29 10:27:43 -0700

    Add max speed to square movement.

--- sketch.js
+++ sketch.js
@@ -20,22 +20,37 @@ function windowResized() {
 function Target(){
     this.x = width/2;
     this.y = height/2;
+    this.maxSpeed = 15;
     this.diameter = 50;
+    this.variation = 0.20;
     this.Xspeed = 3;
     this.Yspeed = 3;
 
     this.move = function() {
+
         if (this.x > windowWidth - this.diameter) {
-            this.Xspeed *= -1;
+            //add slight variation
+            this.Xspeed *= (1 + random(0, this.variation)) * -1;
+            this.x = windowWidth - this.diameter - 1;
         }
         if (this.x < 0) {
-            this.Xspeed *= -1;
+            this.Xspeed *= (1 + random(0, this.variation)) * -1;
+            this.x = 1;
         }
         if (this.y > windowHeight - this.diameter) {
-            this.Yspeed *= -1;
+            this.Yspeed *= (1 + random(0, this.variation)) * -1;
+            this.y = windowHeight - this.diameter -1;
         }
         if (this.y < 0) {
-            this.Yspeed *= -1;
+            this.Yspeed *= (1 + random(0, this.variation)) * -1;
+            this.y = 1;
+        }
+
+        if(this.Xspeed > this.maxSpeed) {
+            this.Xspeed = this.maxSpeed;
+        }
+        if(this.Yspeed > this.maxSpeed) {
+            this.Yspeed = this.maxSpeed;
         }
         this.x += this.Xspeed;
         this.y += this.Yspeed;
