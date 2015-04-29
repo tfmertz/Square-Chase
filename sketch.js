8b94aa42de9c91b9f939ff8e95ca8a6ca1dbfc8b
Tanner Heffner & Connor Abdelnoor & Tom Mertz <student@epicodus.com>
2015-04-29 09:50:39 -0700

    Include movement function from p5 library.

--- sketch.js
+++ sketch.js
@@ -1,13 +1,34 @@
+var square;
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
     background(0);
+    square = new Target();
 }
 
 function draw() {
-    rect(width/2-25, height/2-25, 50, 50);
+
+    square.move();
+    square.display();
 }
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
     background(0);
+}
+
+function Target(){
+    this.x = width/2;
+    this.y = height/2;
+    this.diameter = random(10,30);
+    this.speed = 5;
+
+    this.move = function() {
+        this.x += random(-this.speed, this.speed);
+        this.y += random(-this.speed, this.speed);
+    };
+
+    this.display = function() {
+        rect(this.x, this.y, this.diameter, this.diameter);
+    }
 }
